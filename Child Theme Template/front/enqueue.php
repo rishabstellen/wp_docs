2f4255a0c7b1fdcd1388622411e3d63a9bc30fca
Rishab Singla <rishabstelleninfotech@gmail.com>
2017-12-14 09:46:03 +0530

    Office Update

--- Child Theme Template/front/enqueue.php
+++ Child Theme Template/front/enqueue.php
@@ -5,21 +5,23 @@ function import_styles(){
 	
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
-	wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+	wp_enqueue_style( 'load-fa', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' ); 
+
+    wp_enqueue_style( 'bootstrap-css', get_stylesheet_directory_uri() . '/css/bootstrap.min.css', array() );
 
 }
 
 // Scripts
 function import_scripts(){
 	
+	wp_enqueue_script( 'jquery-3.2.1.min', get_stylesheet_directory_uri() . '/js/jquery-3.2.1.min.js' );
+  	wp_enqueue_script( 'bootstrap.min', get_stylesheet_directory_uri() . '/js/bootstrap.min.js' );
+    
+   
 	// Register Script
 	wp_register_script( 'scripts', get_stylesheet_directory_uri()."/js/script.js" );
 
 	// Enqueue Script
 	wp_enqueue_script( 'scripts' );
-    
+
 }
--- Child Theme Template/functions.php
+++ Child Theme Template/functions.php
@@ -1,8 +1,9 @@
 <?php
 
 // Import Functions
-include( get_stylesheet_directory() . '/front/enqueue.php');
+include "front/enqueue.php";
 
 // Action Hooks
 add_action( 'wp_enqueue_scripts', 'import_styles' );
 add_action( 'wp_enqueue_scripts', 'import_scripts' );
+
